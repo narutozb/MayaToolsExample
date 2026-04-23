0d3800ab37e29560f7b793b3cb705a9d15f52e60
narutozb <45474750+narutozb@users.noreply.github.com>
2026-04-23 22:31:01 +0900

    Add loader script for scene tools plugin

---
+++ maya_cpp_plugin_template_202603/template/scripts/load_scene_tools_plugin.py
@@ -0,0 +1,9 @@
+import maya.cmds as cmds
+
+PLUGIN_NAME = "scene_tools_mll"
+
+if not cmds.pluginInfo(PLUGIN_NAME, query=True, loaded=True):
+    cmds.loadPlugin(PLUGIN_NAME)
+    print("Loaded:", PLUGIN_NAME)
+else:
+    print("Already loaded:", PLUGIN_NAME)
