155ac0e9edf209452dbd505d72e8d9e453b613fb
narutozb <45474750+narutozb@users.noreply.github.com>
2026-04-23 18:19:42 +0900

    Add common Maya CMake module for plugin template

---
+++ maya_cpp_plugin_template_202603/template/cmake/MayaCommon.cmake
@@ -0,0 +1,43 @@
+function(configure_maya_mll TARGET_NAME)
+    target_include_directories(${TARGET_NAME}
+        PRIVATE
+            "${MAYA_LOCATION}/include"
+    )
+
+    target_link_directories(${TARGET_NAME}
+        PRIVATE
+            "${MAYA_LOCATION}/lib"
+    )
+
+    target_link_libraries(${TARGET_NAME}
+        PRIVATE
+            Foundation
+            OpenMaya
+            OpenMayaAnim
+            OpenMayaUI
+        )
+
+    target_compile_definitions(${TARGET_NAME}
+        PRIVATE
+            _BOOL
+            REQUIRE_IOSTREAM
+            NT_PLUGIN
+            _CRT_SECURE_NO_WARNINGS
+    )
+
+    if(MSVC)
+        target_compile_options(${TARGET_NAME}
+            PRIVATE
+                /W4
+                /EHsc
+        )
+    endif()
+
+    set_target_properties(${TARGET_NAME}
+        PROPERTIES
+            PREFIX ""
+            SUFFIX ".mll"
+            RUNTIME_OUTPUT_DIRECTORY "${MAYA_OUTPUT_ROOT}/$<CONFIG>"
+            LIBRARY_OUTPUT_DIRECTORY "${MAYA_OUTPUT_ROOT}/$<CONFIG>"
+    )
+endfunction()
