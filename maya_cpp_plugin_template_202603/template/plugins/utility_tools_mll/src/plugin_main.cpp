d9b3cb2503e3b3c0cc6f3acb16c153f97362f6af
narutozb <45474750+narutozb@users.noreply.github.com>
2026-04-24 12:14:13 +0900

    Add plugin main for utility tools plugin

---
+++ maya_cpp_plugin_template_202603/template/plugins/utility_tools_mll/src/plugin_main.cpp
@@ -0,0 +1,36 @@
+#include "plugin_features.h"
+
+#include <maya/MFnPlugin.h>
+#include <maya/MGlobal.h>
+#include <maya/MStatus.h>
+
+namespace {
+constexpr const char* PLUGIN_VENDOR = "narutozb";
+constexpr const char* PLUGIN_VERSION = "0.2.0";
+}
+
+MStatus initializePlugin(MObject obj)
+{
+    MFnPlugin plugin(obj, PLUGIN_VENDOR, PLUGIN_VERSION, "Any");
+
+    MStatus status = registerPluginFeatures(plugin);
+    if (!status) {
+        return status;
+    }
+
+    MGlobal::displayInfo("utility_tools_mll loaded.");
+    return MS::kSuccess;
+}
+
+MStatus uninitializePlugin(MObject obj)
+{
+    MFnPlugin plugin(obj);
+
+    MStatus status = unregisterPluginFeatures(plugin);
+    if (!status) {
+        return status;
+    }
+
+    MGlobal::displayInfo("utility_tools_mll unloaded.");
+    return MS::kSuccess;
+}
